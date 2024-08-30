23c23195bb9c6ce896dca8c6c0ccd22d19be52bc
Manvendra Saini <121102169+Manvendra27saini@users.noreply.github.com>
2024-08-30 07:17:00 +0530

    Update Details.tsx

--- src/components/Campaign/Details.tsx
+++ src/components/Campaign/Details.tsx
@@ -1,193 +1,260 @@
-import React, { useEffect, useState, useMemo, useContext } from "react";
+import React, { useEffect } from "react";
 import {
   Box,
-  Flex,
+  Container,
+  Center,
   Text,
+  Heading,
   Progress,
-  Button,
-  NumberInput,
-  NumberInputField,
+  Flex,
   Table,
   Thead,
   Tbody,
+  Tfoot,
   Tr,
   Th,
   Td,
-  TableContainer,
   TableCaption,
+  TableContainer,
+  Button,
+  NumberInput,
+  NumberInputField,
 } from "@chakra-ui/react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Navigate, Router, useNavigate, useParams } from "react-router-dom";
 import { AppContext } from "../../Context";
-import { useWallet } from "@solana/wallet-adapter-react";
+import Navbar from "../Navbar";
+import { useWallet, useConnection } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useAppSelector } from "../../redux/hook";
 import { TransactionT } from "../../redux/types";
 import { ChatIcon } from "@chakra-ui/icons";
-import { IoIosArrowBack } from "react-icons/io";
-import SideNav from "../SideNav/HalfSide";
+	@@ -33,31 +27,23 @@ import SideNav from "../SideNav/HalfSide";
 
 function Details() {
   const { id }: any = useParams();
-  const { getACampaign, donate, transactionPending } = useContext(AppContext);
-  const recipient = useAppSelector((state) => state.recipient);
-  const navigate = useNavigate();
-  const { publicKey } = useWallet();
-  const [amount, setAmount] = useState<number>(0);
+  const { getACampaign, donate, transactionPending } =
+    React.useContext(AppContext);
 
-  useEffect(() => {
-    if (id) {
-      getACampaign(id);
-    }
-  }, [id, publicKey, getACampaign]);
+  const recipient = useAppSelector((state) => state.recipient);
+  console.log(recipient);
+
+  const navigate = useNavigate();
+
+  const { publicKey } = useWallet();
+
+  const [amount, setAmount] = React.useState<number>(0);
+  React.useMemo(() => {
+    const call = async () => {
+      if (id) {
+        await getACampaign(id);
+      }
+    };
+    call();
+  }, [publicKey, id]);
 
   const amountLeft = recipient.amountRequired - recipient.amountDonated;
   const progress = (recipient.amountDonated / recipient.amountRequired) * 100;
 
-  const parse = (val: string) => parseFloat(val.replace(/^\$/, ""));
-  const format = (val: number) => `$${val}`;
+  const parse: any = (val: string) => val.replace(/^\$/, "");
+  const format = (val: number) => `$` + val;
 
   const handleDonate = async () => {
     await donate(amount);
-  };
-
-  return (
+	@@ -67,13 +53,13 @@ function Details() {
     <SideNav>
       {!publicKey ? (
         <Flex align="center" justify="center" flexDirection="column">
-          <Text fontSize="24px">
+          <Text fontSize={"24px"}>
             Please connect a wallet to see full details and donate
           </Text>
           {/* <WalletMultiButton /> */}
         </Flex>
       ) : (
-        <Flex flexDirection="column" my={8} gap={6}>
+        <Flex flexDirection={"column"} my={8} gap={6}>
           {recipient.name.length > 2 ? (
             <>
               <Flex justify="space-evenly">
-                <IoIosArrowBack />
-                <Text>SolFunding for {recipient.name}</Text>
+	@@ -82,13 +68,13 @@ function Details() {
                 <Box />
               </Flex>
 
-              {/* Funding Details */}
+              {/* funding details */}
               <Box
                 color="black"
                 py={3}
                 mx={8}
                 px={8}
-                borderRadius="15px"
+                borderRadius={"15px"}
                 h={170}
                 bgColor="white"
                 gap={6}
-                cursor="pointer"
-              >
-                <Flex color="#5E5E5E" fontWeight={600} justify="space-between">
-                  <Text>{recipient.name}</Text>
-                  <Text>{Math.floor(progress)}%</Text>
-                </Flex>
+	@@ -101,7 +87,6 @@ function Details() {
                 <Flex color="#353535" mt={1}>
                   0.334 SOL
                 </Flex>
+
                 <Flex
                   color="#1935C4"
                   fontWeight={600}
-                  mt={3}
-                  justify="space-between"
-                >
-                  <Text>${recipient?.amountDonated}</Text>
-                  <Text>${recipient?.amountRequired}</Text>
-                </Flex>
+	@@ -114,38 +99,17 @@ function Details() {
                 <Progress color="#1935C4" value={Math.floor(progress)} />
               </Box>
 
-              {/* Extra Funding Details */}
+              {/* extra funding details */}
               <NumberInput
                 mx={8}
                 value={format(amount)}
                 onChange={(value: string) => setAmount(parse(value))}
                 defaultValue={0}
                 clampValueOnBlur={false}
-                h="40px"
+                h={"40px"}
               >
                 <NumberInputField />
               </NumberInput>
+
+              {/* <Box
+                color="black"
+                py={1}
+                mx={8}
+                px={8}
+                borderRadius={"15px"}
+                h={49}
+                bgColor="white"
+              >
+                {" "}
+                <Flex
+                  color="#1935C4"
+                  fontWeight={600}
+                  mt={3}
+                  justify="space-between"
+                >
+                  <Text>$10,000</Text>
+                  <Text>30 SOL</Text>
+                </Flex>
+              </Box> */}
               <Button
                 mx={8}
                 py={4}
-                bgColor="#4C3FE7"
-                color="white"
-                border="none"
-                onClick={handleDonate}
-                isDisabled={amount < 0.1 || transactionPending}
-                isLoading={transactionPending}
+	@@ -158,12 +122,13 @@ function Details() {
               >
                 Send {recipient.name} some Sol
               </Button>
-
               {/* Message */}
               <Flex
                 justify="flex-end"
                 mx={3}
                 gap={3}
-                cursor="pointer"
+                cursor={"pointer"}
                 onClick={() => navigate("/message")}
               >
                 <Text>Send a Message</Text>
-                <ChatIcon boxSize={6} />
-              </Flex>
-
-              <Text mx={8}>Transaction History</Text>
-              <Box>
-                <Transactions />
+	@@ -176,102 +141,24 @@ function Details() {
               </Box>
             </>
           ) : (
-            <Heading textAlign="center">
-              Oops!... You can't SolFund yourself
+            <Heading textAlign={"center"}>
+              Opps!... You can't solFund yourself
             </Heading>
           )}
         </Flex>
       )}
     </SideNav>
   );
+  // return (
+  //   <Container
+  //     maxW={{ base: "90%", md: "60%" }}
+  //     h={"100vh"}
+  //     my={8}
+  //     border={"1px solid white"}
+  //     py={10}
+  //     px={10}
+  //   >
+  //     {!publicKey ? (
+  //       <Flex align="center" justify="center" flexDirection="column">
+  //         <Text fontSize={"24px"}>
+  //           Please connect a wallet to see full details and donate
+  //         </Text>
+  //         <WalletMultiButton />
+  //       </Flex>
+  //     ) : (
+  //       <>
+  //         <Navbar />
+  //         {recipient.name.length > 2 ? (
+  //           <>
+  //             <Heading textAlign="center">
+  //               SolFunding for {recipient.name}
+  //             </Heading>
+  //             <Text>{recipient.description}</Text>
+  //             <Box mt={"40px"}>
+  //               <Progress value={progress} />
+  //               <Flex justify={"flex-end"}>
+  //                 <Text mt={4}>
+  //                   $ {amountLeft.toLocaleString()} to reach goal🎉
+  //                 </Text>
+  //               </Flex>
+  //               <Flex justify={"center"} gap={3} flexDirection={"column"}>
+  //                 <NumberInput
+  //                   value={format(amount)}
+  //                   onChange={(value: string) => setAmount(parse(value))}
+  //                   defaultValue={0}
+  //                   clampValueOnBlur={false}
+  //                   h={"40px"}
+  //                 >
+  //                   <NumberInputField />
+  //                 </NumberInput>
+  //                 <Button
+  //                   py={7}
+  //                   px={5}
+  //                   onClick={handleDonate}
+  //                   isDisabled={amount < 0.1 || transactionPending}
+  //                   isLoading={transactionPending}
+  //                 >
+  //                   Send {recipient.name} some Sol
+  //                 </Button>
+  //                 {/* Message */}
+  //                 <Flex
+  //                   justify="flex-end"
+  //                   gap={3}
+  //                   cursor={"pointer"}
+  //                   onClick={() => navigate("/message")}
+  //                 >
+  //                   <Text>Send a Message</Text>
+  //                   <ChatIcon color="primary.50" boxSize={6} />
+  //                 </Flex>
+  //               </Flex>
+  //             </Box>
+  //             {/* transaction */}
+  //             <Box h={"100px"} />
+  //             <Transactions />
+  //           </>
+  //         ) : (
+  //           <Heading textAlign={"center"}>
+  //             Opps!... You can't solFund yourself
+  //           </Heading>
+  //         )}
+  //       </>
+  //     )}
+  //   </Container>
+  // );
 }
 
-function Transactions() {
-  const transaction = useAppSelector((state) => state.transaction);
+export default Details;
+
+export function Transactions() {
+  const transaction = useAppSelector((state) => state.transction);
 
   return (
     <TableContainer
       mx={8}
-      h="200px"
-      overflowY="scroll"
+      h={"200px"}
+      overflowY={"scroll"}
       css={{
         "&::-webkit-scrollbar": {
           display: "none", // Hide scrollbar for Chrome, Safari, and Opera
-        },
-        scrollbarWidth: "none", // Hide scrollbar for Firefox
-        msOverflowStyle: "none", // Hide scrollbar for Internet Explorer and Edge
-      }}
-    >
-      <Table variant="simple">
-        <TableCaption>Transaction details</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Signature</Th>
-            <Th>Date</Th>
-            <Th>Status</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {transaction?.map((val: TransactionT) => (
-            <Tr key={val.transactionNo}>
-              <Td>{val.signature.slice(0, 30)}...</Td>
-              <Td>{val.time.toISOString()}</Td>
-              <Td>{val.status}</Td>
-            </Tr>
-          ))}
+	@@ -300,6 +187,7 @@ export function Transactions() {
         </Tbody>
       </Table>
     </TableContainer>
+    //nothingS
   );
 }
-
-export default Details;
