8a2a768f6bbe537e3a26b2bb3b5e1526ee5e0eb1
Manvendra Saini <121102169+Manvendra27saini@users.noreply.github.com>
2024-08-30 06:50:54 +0530

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,1 +1,21 @@
+import React from "react";
+import { Text, Flex } from "@chakra-ui/react";
 
+function Footer() {
+  return (
+    <Flex
+      justify={{ md: "space-evenly" }}
+      flexDirection={{ base: "column", md: "row" }}
+      align={{ base: "center" }}
+      mt={8}
+    >
+      <Flex>
+        <Text size="24px" fontWeight={500}>
+          Powered by Solana
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Footer;
